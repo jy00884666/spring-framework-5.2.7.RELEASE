549853d6fbed1bc27fe46d34f74212ec4feee3a2
jy00884666 <87419590@qq.com>
2022-08-02 12:34:01 +0800

    20220802

--- spring-zztext-test/src/main/java/com/test/MyText.java
+++ spring-zztext-test/src/main/java/com/test/MyText.java
@@ -3,12 +3,13 @@ package com.test;
 import com.test.bean.Person;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-/**
- * Ctrl + Alt + Shift + u 查看类图结构
- */
+/*Ctrl + Alt + Shift + u 查看类图结构*/
 public class MyText {
 	
 	public static void main(String[] args) {
+		// 打印默认编码
+		//System.out.println("打印默认编码:"+System.getProperty("file.encoding"));
+		
 		// 创建Spring容器
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Person.class);
 		// 获取对象Bean
